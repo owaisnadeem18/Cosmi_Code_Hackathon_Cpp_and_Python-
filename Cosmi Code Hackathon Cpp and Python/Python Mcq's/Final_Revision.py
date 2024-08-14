c15cc8c0cbd19c9f09c33d1bf888374079740f9c
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2024-08-14 20:42:31 +0500

    Add files via upload

---
+++ Cosmi Code Hackathon Cpp and Python/Python Mcq's/Final_Revision.py
@@ -0,0 +1,21 @@
+# a = 15
+# b = 4
+# result = a / b
+# print((result))
+
+# print(-10 // 3)  # Output: -4
+
+# my_dict = {'a': 1, 'b': 2, 'c': 3}
+# value = my_dict.get('d', 4)
+# print(value)
+
+# print(my_dict)
+
+# lisT = [4,  2,5,2 ,5 ,3]
+ 
+# print(type(lisT))
+
+
+list = [1, 2, 3, 4]
+# print(list[::-1])
+print(list[::])
